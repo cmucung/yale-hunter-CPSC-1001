96e3ce2e41928108885367407325c2275231037b
helen huang <85393862+hailuen@users.noreply.github.com>
2025-11-24 21:12:46 -0600

    Implement canScare method in Professor class
    Added method to check for nearby students.

--- Professor.java
+++ Professor.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 public class Professor {
     // position and size
     private int x, y, speed, width, height;
+    private boolean studentNearby;
 
     // constructor
     public Professor() {
-        // default starting position and movement speed
+        this.studentNearby = false;
         
     }
-    
+
     // movement based on a single key character (W/A/S/D)
     public void move(char key) {
         switch (Character.toLowerCase(key)) {
@@ -32,8 +33,19 @@ public class Professor {
         }
     }
 
-    // simple accessors
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public Rectangle getBounds() { return new Rectangle(x, y, width, height); }
+    public boolean canScare(grid[][] students){
+        if (students[x + 1][y].hasStudent()){
+            studentNearby = true;
+        } else if (students[x - 1][y].hasStudent()){
+            studentNearby = true;
+        } else if (students[x][y + 1].hasStudent()){
+            studentNearby = true;
+        } else if (students[x][y - 1].hasStudent()){
+            studentNearby = true;
+        } else {
+            studentNearby = false;
+        }
+        return studentNearby;
+    }
+
 }
