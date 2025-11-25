b62bdd56c00dfa3cd55780ad3d55c990da8ddf83
helen huang <85393862+hailuen@users.noreply.github.com>
2025-11-25 15:36:35 -0600

    Add files via upload

---
+++ main/UtilityTool.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+public class UtilityTool {
+    public BufferedImage scaleImage(BufferedImage original, int width, int height) {
+        // Always use TYPE_INT_ARGB to preserve colors properly
+        BufferedImage scaledImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = scaledImage.createGraphics();
+        g2.drawImage(original, 0, 0, width, height, null);
+        g2.dispose();
+        return scaledImage;
+    }
+}
