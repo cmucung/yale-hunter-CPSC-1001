7436e2ab87adeee0c9d383cc475a9f13be8c429d
helen huang <85393862+hailuen@users.noreply.github.com>
2025-11-29 23:30:04 -0500

    Replace Student entity with SuperObject in GamePanel
    Refactor GamePanel to integrate SuperObject and remove Student entity.

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -2,20 +2,20 @@ package main;
 import javax.swing.JPanel;
 
 import entity.Player;
-import entity.Student;
+import object.SuperObject;
 import tile.TileManager;
 
 import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 
 public class GamePanel extends JPanel implements Runnable {
-    // Game panel code here
-    // screen settings
+    // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
-    final int scale = 3;
+    final int scale = 3; // scale x3
    
     public final int tileSize = originalTileSize * scale; // 48x48 tile
     public final int maxScreenCol = 16;
@@ -23,33 +23,43 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // WORLD SETTINGS
     public int maxWorldCol;
     public int maxWorldRow;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
     public final int maxMap = 10;
     public int currentTimeMap = 0;
 
+    // FULL SCREEN SETTINGS
+    int screenWidth2 = screenWidth;
+    int screenHeight2 = screenHeight;
+    BufferedImage tempScreen;
+    Graphics2D g2;
 
     // FPS
     int FPS = 60;
 
     // SYSTEM
-    public TileManager tileM = new TileManager(this);
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler(this);
-    public UI ui = new UI(this);
-    Thread gameThread;
+    Sound music = new Sound();
+    Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+    Config config = new Config(this);
+    Thread gameThread;
 
-    // ENTITY
+    // ENTITY AND OBJECT
     public Player player = new Player(this, keyH);
-    public Student[] students = new Student[4]; // Array to hold multiple students
+    public SuperObject obj[] = new SuperObject[10];
 
     // GAME STATE
     public int gameState;
     public final int titleState = 0;
     public final int playState = 1;
     public final int pauseState = 2;
+    public final int optionsState = 3;
+    public final int gameOverState = 4;
 
 
     // Set player's default position
@@ -58,7 +68,8 @@ public class GamePanel extends JPanel implements Runnable {
     int playerSpeed = 4;
 
 
-    public GamePanel() {
+    // CONSTRUCTOR
+    public GamePanel() { 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
@@ -67,59 +78,19 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void setupGame() {
+        aSetter.setObject();
         gameState = titleState;
-        
-        // Initialize students with valid positions
-        for (int i = 0; i < students.length; i++) {
-            students[i] = new Student(this);
-            students[i].setValidPosition();
-        }
+
+        // FOR FULL SCREEN)
+        tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB); // blank screen
+        g2 = (Graphics2D) tempScreen.getGraphics();
     }
 
 
     public void startGameThread() {
-        setupGame();
         gameThread = new Thread(this);
         gameThread.start();
     }
-
-    /*
-    @Override
-    public void run() {
-        // Game loop code here
-        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
-
-
-        while (gameThread != null) {
-
-
-            update();
-
-
-            repaint();
-
-
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000;
-
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-
-                Thread.sleep((long) remainingTime);
-
-
-                nextDrawTime += drawInterval;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    } 
-    */
 
     public void run() {
         double drawInterval = 1000000000 / FPS; // 0.01666 seconds
@@ -153,42 +124,106 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void update() {
         if (gameState == playState) {
+            // PLAYER
             player.update();
-            
-            // Update all students
-            for (int i = 0; i < students.length; i++) {
-                if (students[i] != null) {
-                    students[i].update();
-                }
-            }
+
+            // NPC
         }
     }
 
+    // FOR FULL SCREEN
+    public void drawToTempScreen() {
+        // TITLE
+        if (gameState == titleState) {
+            ui.draw(g2);
+        }
+
+        else {
+            // TILE 
+            tileM.draw(g2);
+
+            // OBJECT
+            for (int i = 0; i < obj.length; i++) {
+                if (obj[i] != null) {
+                    obj[i].draw(g2, this);
+                }
+            }
+            // PLAYER
+            player.draw(g2);
+
+            // UI
+            ui.draw(g2);
+        }
+    }
+
+    public void drawToScreen() {
+        Graphics g = getGraphics();
+        g.drawImage(tempScreen, 0, 0, screenWidth2, screenHeight2, null);
+        g.dispose();
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+        // debug
         // Draw title screen
         if (gameState == titleState) {
             ui.draw(g2);
         } else {
-            // Draw tiles first (background)
+            // TILE 
             tileM.draw(g2);
-        
-            // Draw students
-            for (int i = 0; i < students.length; i++) {
-                if (students[i] != null) {
-                    students[i].draw(g2, player);
+
+            // OBJECT
+            for (int i = 0; i < obj.length; i++) {
+                if (obj[i] != null) {
+                    obj[i].draw(g2, this);
                 }
             }
-            
-            // Draw player second (foreground)
+            // PLAYER
             player.draw(g2);
 
-            // Draw UI last (on top of everything)
+            // UI
             ui.draw(g2);
         }
 
         g2.dispose();
+    }
+
+    public void playMusic(int i) {
+        
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+
+    public void stopMusic() {
+        music.stop();
+    }
+
+    public void playSE(int i) {
+        se.setFile(i);
+        se.play();
+    }
+
+    public void retry() {
+        // reset player position and state
+        player.setDefaultValues();
+        
+        // reset time
+        ui.playTime = 0;
+        
+        // reset objects
+        aSetter.setObject();
+    }
+
+    public void restart() {
+        // reset player position and state
+        player.setDefaultValues();
+        
+        // reset time
+        ui.playTime = 0;
+        
+        // reset objects
+        aSetter.setObject();
     }
 }
