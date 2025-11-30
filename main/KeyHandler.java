471a2f5506991b61065333096cf36c63f57bca35
helen huang <85393862+hailuen@users.noreply.github.com>
2025-11-29 23:30:19 -0500

    Refactor KeyHandler to include enterPressed and states
    Added enterPressed boolean and refactored key handling methods for better state management.

--- main/KeyHandler.java
+++ main/KeyHandler.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
     GamePanel gp;
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
     public boolean scarePressed;
     
     public KeyHandler(GamePanel gp) {
@@ -23,93 +23,215 @@ public class KeyHandler implements KeyListener {
 
         // TITLE STATE
         if (gp.gameState == gp.titleState) {
+            titleState(code);
+        }
 
-            // CHECK WHICH SCREEN WE ARE ON
-            if (gp.ui.titleScreenState == 0) {
-                if (code == KeyEvent.VK_W) {
-                gp.ui.commandNum--;
-                if (gp.ui.commandNum < 0) {
-                    gp.ui.commandNum = 2;
-                    }
-                } 
-                if (code == KeyEvent.VK_S) {
-                    gp.ui.commandNum++;
-                    if (gp.ui.commandNum > 2) {
-                        gp.ui.commandNum = 0;
-                    }
-                }
-                if (code == KeyEvent.VK_ENTER) {
-                    if (gp.ui.commandNum == 0) {
-                        gp.ui.titleScreenState = 1;
-                        gp.ui.commandNum = 0;
-                    }
-                    if (gp.ui.commandNum == 1) {
-                        // instructions screen
-                        gp.ui.titleScreenState = 2;
-                        gp.ui.commandNum = 0;
-                    }
-                    if (gp.ui.commandNum == 2) {
-                        // exit the game
-                        System.exit(0);
-                    }
-                }
+        // PLAY STATE 
+        else if (gp.gameState == gp.playState) {
+            playState(code);
+        }
+
+        // OPTIONS STATE
+        else if (gp.gameState == gp.optionsState) {
+           optionsState(code);
+        }
+
+        // GAME OVER STATE
+        else if (gp.gameState == gp.gameOverState) {
+           gameOverState(code);
+        }
+
+    }
+
+    public void optionsState(int code) {
+        if (code == KeyEvent.VK_ESCAPE) {
+            gp.gameState = gp.playState;
+        }
+
+        if (code == KeyEvent.VK_ENTER) {
+            enterPressed = true;
+        }
+
+        int maxCommandNum = 0;
+        switch(gp.ui.subState) {
+            case 0: maxCommandNum = 3; break;
+            case 1: maxCommandNum = 1; break;
+        }
+
+        if (code == KeyEvent.VK_W) {
+            gp.ui.commandNum--;
+            gp.playSE(2);
+            if (gp.ui.commandNum < 0) {
+                gp.ui.commandNum = maxCommandNum;
             }
-            else if (gp.ui.titleScreenState == 1) {
-                if (code == KeyEvent.VK_W) {
-                    gp.ui.commandNum--;
-                    if (gp.ui.commandNum < 0) {
-                        gp.ui.commandNum = 2;
-                    }
-                } 
-                if (code == KeyEvent.VK_S) {
-                    gp.ui.commandNum++;
-                    if (gp.ui.commandNum > 2) {
-                        gp.ui.commandNum = 0;
-                    }
-                }
-                if (code == KeyEvent.VK_ENTER) {
-                    if (gp.ui.commandNum == 0) {
-                        System.out.println("Play as Professor Tim Barron");
-                        gp.gameState = gp.playState;
-                        // gp.playMusic(0);
-                    }
-                    if (gp.ui.commandNum == 1) {
-                        System.out.println("Play as Professor Ozan Erat");
-                    }
-                    if (gp.ui.commandNum == 2) {
-                        gp.ui.titleScreenState = 0;
-                    }
-                }
+        }
+
+        if (code == KeyEvent.VK_S) {
+            gp.ui.commandNum++;
+            gp.playSE(2);
+            if (gp.ui.commandNum > maxCommandNum) {
+                gp.ui.commandNum = 0;
             }
-            else if (gp.ui.titleScreenState == 2) {
-                if (code == KeyEvent.VK_ENTER) {
-                    if (gp.ui.commandNum == 0) {
-                        gp.ui.titleScreenState = 0;
-                    }
+        }
+
+        if (code == KeyEvent.VK_A) {
+            if (gp.ui.subState == 0) {
+                if (gp.ui.commandNum == 0 && gp.music.volumeScale > 0) {
+                    gp.music.volumeScale--;
+                    gp.music.checkVolume();
+                    gp.playSE(2);
+                }
+                if (gp.ui.commandNum == 1 && gp.se.volumeScale > 0) {
+                    gp.se.volumeScale--;
+                    gp.se.checkVolume();
+                    gp.playSE(2);
                 }
             }
         }
 
-        // PLAY STATE 
-        if (gp.gameState == gp.playState) {
+        if (code == KeyEvent.VK_D) {
+            if (gp.ui.subState == 0) {
+                if (gp.ui.commandNum == 0 && gp.music.volumeScale < 5) {
+                    gp.music.volumeScale++;
+                    gp.music.checkVolume();
+                    gp.playSE(2);
+                }
+                if (gp.ui.commandNum == 1 && gp.se.volumeScale < 5) {
+                    gp.se.volumeScale++;
+                    gp.se.checkVolume();
+                    gp.playSE(2);
+                }
+            }
+        }
+    }
+    public void titleState(int code) {
+        // MAIN MENU
+        if (gp.ui.titleScreenState == 0) {
             if (code == KeyEvent.VK_W) {
-                upPressed = true;
-            }
+            gp.ui.commandNum--;
+            if (gp.ui.commandNum < 0) {
+                gp.ui.commandNum = 2;
+                }
+            } 
             if (code == KeyEvent.VK_S) {
-                downPressed = true;
+                gp.ui.commandNum++;
+                if (gp.ui.commandNum > 2) {
+                    gp.ui.commandNum = 0;
+                   }
             }
-            if (code == KeyEvent.VK_A) {
-                leftPressed = true;
+            if (code == KeyEvent.VK_ENTER) {
+                if (gp.ui.commandNum == 0) {
+                    gp.ui.titleScreenState = 1;
+                    gp.ui.commandNum = 0;
+                }
+                if (gp.ui.commandNum == 1) {
+                    // instructions screen
+                    gp.ui.titleScreenState = 2;
+                    gp.ui.commandNum = 0;
+                }
+                if (gp.ui.commandNum == 2) {
+                    // exit the game
+                    System.exit(0);
+                }
             }
-            if (code == KeyEvent.VK_D) {
-                rightPressed = true;
+        }
+
+        // SECOND SCREEN --- SELECT CHARACTER  
+        else if (gp.ui.titleScreenState == 1) {
+            if (code == KeyEvent.VK_W) {
+                gp.ui.commandNum--;
+                if (gp.ui.commandNum < 0) {
+                    gp.ui.commandNum = 2;
+                }
+            } 
+            if (code == KeyEvent.VK_S) {
+                gp.ui.commandNum++;
+                if (gp.ui.commandNum > 2) {
+                    gp.ui.commandNum = 0;
+                }
             }
-            if (code == KeyEvent.VK_X) {
-                scarePressed = true;
+            if (code == KeyEvent.VK_ENTER) {
+                // PROFESSOR TIM BARRON
+                if (gp.ui.commandNum == 0) {
+                    System.out.println("Play as Professor Tim Barron");
+                    gp.gameState = gp.playState;
+                    gp.playMusic(0);
+                }
+                // PROFESSOR OZAN ERAT
+                if (gp.ui.commandNum == 1) {
+                    System.out.println("Play as Professor Ozan Erat");
+                }
+                // BACK
+                if (gp.ui.commandNum == 2) {
+                    gp.ui.titleScreenState = 0;
+                }
             }
-       }
+        }
+        // INSTRUCTIONS SCREEN
+        else if (gp.ui.titleScreenState == 2) {
+            // BACK
+            if (code == KeyEvent.VK_ENTER) {
+                if (gp.ui.commandNum == 0) {
+                    gp.ui.titleScreenState = 0;
+                }
+            }
+        }
     }
 
+    public void playState(int code) {
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+        if (code == KeyEvent.VK_X) {
+            scarePressed = true;
+        }
+
+        if (code == KeyEvent.VK_ESCAPE) {
+            gp.gameState = gp.optionsState;
+        }
+
+    }
+
+    public void gameOverState(int code) {
+        if (code == KeyEvent.VK_W) {
+            gp.ui.commandNum--;
+            if (gp.ui.commandNum < 0) {
+                gp.ui.commandNum = 1;
+            }
+            gp.playSE(2);
+        }
+        if (code == KeyEvent.VK_S) {
+            gp.ui.commandNum++;
+            if (gp.ui.commandNum > 1) {
+                gp.ui.commandNum = 0;
+            }
+            gp.playSE(2);
+        }
+
+        if (code == KeyEvent.VK_ENTER) {
+            if (gp.ui.commandNum == 0) {
+                gp.retry();
+                gp.gameState = gp.playState;
+                gp.playMusic(0);
+            }
+            else if (gp.ui.commandNum == 1) {
+                gp.stopMusic();
+                gp.ui.titleScreenState = 0;
+                gp.gameState = gp.titleState;
+                gp.restart();
+            }
+            gp.playSE(3);
+        }
+    }
 
     @Override
     public void keyReleased(KeyEvent e) {
