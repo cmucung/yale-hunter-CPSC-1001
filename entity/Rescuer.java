bdfb98237ef5e628e38896651c7911d15d417ef6
Carmen Ung <carmenung168@gmail.com>
2025-11-30 19:36:21 -0500

    Update Rescuer.java

--- entity/Rescuer.java
+++ entity/Rescuer.java
@@ -3,5 +3,16 @@ import java.util.List;
 import java.awt.*;
 
 public class Rescuer extends Student {
+  private Entity frozenTarget = null; // frozen student 
+  private int unfreezeCounter = 0; // keeps track of time standing next to target to unfreeze 
+  private boolean isUnfreezing = false; 
 
+  public Rescuer(GamePanel gp) {
+    super.gp();
+    speed = 2;
+  }
+
+  @Override 
+  public void update() {
+    
 }
